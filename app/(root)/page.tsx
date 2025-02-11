bef1831eb93861e480aacba3ae9c60132d0ecd39
Gabriel Gravina <gabrielgravina01@gmail.com>
2025-02-11 16:15:05 -0300

    Creating vertical menu for smaller screen devices

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { resolve } from "path";
+
+export const metadata = { title: 'Home', };
+
+const Homepage = async () => {
+  return (
+    <> 
+      Prostore
+    </> 
+  );
+};
+export default Homepage;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+import { FlatCompat } from '@eslint/eslintrc';
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
 });
-
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next'],
+    plugins: ['import-newlines'],
 
+    rules: {
+      semi: ["error"],
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      "object-curly-newline": ["error", { "multiline": true }],
+      "object-curly-spacing": ["error", "always"],
+      'import-newlines/enforce': 'error',
+      
+      'react/jsx-indent-props': ['error', 2], // Indentação de 2 espaços para props
+      'react/jsx-indent': ['error', 2], // Indentação geral de 2 espaços para JSX
+
+      'react/jsx-closing-bracket-location': ['error', 'line-aligned'],
+      'react/jsx-closing-tag-location': 'error',
+      'react/jsx-wrap-multilines': 'error',
+      'react/jsx-first-prop-new-line': ['error', 'multiline-multiprop'], // Ajustado
+      'react/jsx-max-props-per-line': ['error', { maximum: 2, when: 'multiline' }] // Ajustado
+      
+    },
+    settings: { next: { rootDir: 'packages/my-app/', }, },
+  }),
+];
 export default eslintConfig;
