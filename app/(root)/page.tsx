727584b9784c9edad9c166f0f7f72009ab38df31
Gabriel Gravina <00119110344@pq.uenf.br>
2025-02-25 15:52:35 -0300

    feat: creating prisma scheme

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,14 @@
 import { resolve } from "path";
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 
 export const metadata = { title: 'Home', };
 
 const Homepage = async () => {
   return (
     <> 
-      Prostore
+      <ProductList data={sampleData.products} title='Newest Arrival' limit={4} />
     </> 
   );
 };
-export default Homepage;
+export default Homepage; 
